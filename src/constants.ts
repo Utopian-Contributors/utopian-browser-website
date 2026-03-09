00e40f49a0ce7ba225286b68111eae174a4f5fd8
Ludwig Schubert <ludwigschubi1@gmail.com>
2026-03-10 04:35:48 +0800

    fix: email

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const WAITLIST_EMAIL =
-  "mailto:ludwig@utopiancontrib?subject=I%20would%20like%20to%20start%20using%20the%20Utopian%20Browser%20as%20early%20as%20possible!";
+  "mailto:ludwig@utopiancontrib.com?subject=I%20would%20like%20to%20start%20using%20the%20Utopian%20Browser%20as%20early%20as%20possible!";
