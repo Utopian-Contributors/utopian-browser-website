a07eb9326e85c1993892a7de1f9961fd8d90dc07
Ludwig Schubert <ludwigschubi1@gmail.com>
2026-03-21 19:21:45 +0800

    feat: migrate tribe sdk to npm package

---
+++ src/tribe.ts
@@ -0,0 +1,7 @@
+import { Tribe } from "@tribecloud/sdk";
+
+export const tribe = new Tribe({
+  siteId: "cmmuqfheg00t60zo7ltf2hlfg",
+  _baseUrl: "https://api.tribe.utopian.build",
+  autoTrack: true,
+});
